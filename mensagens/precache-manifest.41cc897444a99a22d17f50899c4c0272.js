acfc7bf1472014286e9b531c16c8f48d2fbe4cb6
Glauber Holanda <gholanda@uolinc.com>
2018-12-05 19:01:40 -0200

    Mensagens B103

---
+++ mensagens/precache-manifest.41cc897444a99a22d17f50899c4c0272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "780fbfa11367f21591e7",
+    "url": "/static/js/main.780fbfa1.chunk.js"
+  },
+  {
+    "revision": "7c78adb3db02823816a4",
+    "url": "/static/js/1.7c78adb3.chunk.js"
+  },
+  {
+    "revision": "780fbfa11367f21591e7",
+    "url": "/static/css/main.0c90e8c8.chunk.css"
+  },
+  {
+    "revision": "1fc4eb2da44e1adc38594a5927a5fcdb",
+    "url": "/index.html"
+  }
+];
